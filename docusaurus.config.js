8e8e3570c13ae001a1d63ab511be56da9872e047
Davide Bianchi <davide.bianchi@mia-platform.eu>
2021-10-29 14:52:17 +0000

    feat: upgrade docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -264,7 +264,6 @@ const config = {
           customCss: require.resolve("./src/css/custom.css"),
         },
         sitemap: {
-          cacheTime: 600 * 1000, // 600 sec - cache purge period
           changefreq: "weekly",
           priority: 0.5,
         },
