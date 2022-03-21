d34121fcdba8458e654d9f2aa256356bb4f9a3c5
Federico Maggi <federico.maggi@mia-platform.eu>
2022-03-21 17:53:51 +0100

    migrate gtm

--- src/constants.js
+++
@@ -1,9 +0,0 @@
-const cookieBannerConf = {
-  message:
-    "This site uses proprietary and third-party cookies in order to improve the browsing experience.",
-  cookie: "user-has-accepted-cookies",
-  moreInfoLink: "https://www.mia-platform.eu/img/Privacy_Policy_Website_EN.pdf",
-  moreInfoMessage: "For more information, read the Privacy Policy",
-};
-
-export {cookieBannerConf};
