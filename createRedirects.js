f406bf36249e6d028f7f2d824f16a153ec55abb3
Davide Bianchi <davide.bianchi@mia-platform.eu>
2022-06-07 10:02:32 +0000

    feat: check files not in sidebar

--- createRedirects.js
+++ createRedirects.js
@@ -6,15 +6,22 @@ const redirectPaths = {
   "/docs/development_suite/api-console/api-design/endpoints": "/development_suite/api-console/api-design/esponi_api",
   "/docs/runtime_suite/ses-mail-notification-service/configuration": "/docs/runtime_suite/mail-notification",
   "/docs/guidelines/git_vademecum": "/dev_ops_guide/git_vademecum/",
-  "/docs/development_suite/api-console/api-design/services": "/docs/guidelines/communication_between_services_mp4"
+  "/docs/development_suite/api-console/api-design/services": "/docs/guidelines/communication_between_services_mp4",
+  "/docs/runtime_suite_tools/miactl/overview": "/docs/development_suite/miactl/overview",
+  "/docs/runtime_suite/push-notification-manager/configuration": "/docs/runtime_suite/notifications-manager",
+  "/docs/runtime_suite/push-notification-sender/overview": "/docs/runtime_suite/push_notifications_platform_4",
+  "/docs/runtime_suite/envoy-api-gateway/filters": "/docs/runtime_suite/api-gateway-envoy/filters",
+  "/docs/runtime_suite/envoy-api-gateway/overview": "/docs/runtime_suite/api-gateway-envoy/overview",
+  "/docs/fast_data/overview": "/docs/runtime_suite/event-source-management",
+  "/docs/runtime_suite/pdf-service/overview": "/docs/runtime_suite/pdf-service",
 };
 
 const createRedirects = (path) => {
   const redirectPath = redirectPaths[path];
 
   if (redirectPath) return [redirectPath];
-  
-  // Redirects all url without docs to docs/../..  
+
+  // Redirects all url without docs to docs/../..
   return [path.replace(/^\/docs/g, "")];
 };
 
--- new-release-note.js
+++ new-release-note.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const newVersion = process.argv.slice(-1)[0]
     .replace(/^v/, '');
-let unreleased = fs.readFileSync('./docs/release_notes/unreleased/unreleased.md', {encoding: 'utf8'});
+let unreleased = fs.readFileSync('./release-note-unreleased.md', {encoding: 'utf8'});
 unreleased = unreleased.replace(/_VERSION_/g, newVersion)
 const desiredMinorFolder = newVersion.split('.').slice(0, -1).join('-')
 const newFolder = `RN_v${desiredMinorFolder}`
