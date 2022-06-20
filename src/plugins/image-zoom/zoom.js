4ff9f7c589b50abaf475d0a3100f4b1f5c49bede
Davide Bianchi <davide.bianchi@mia-platform.eu>
2022-06-20 12:28:10 +0000

    feat: add image zoom

---
+++ src/plugins/image-zoom/zoom.js
@@ -0,0 +1,25 @@
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
+import mediumZoom from 'medium-zoom';
+
+const className = '.markdown img'
+
+export default (function () {
+  if (!ExecutionEnvironment.canUseDOM || window === undefined) {
+    return
+  }
+
+  return {
+    onRouteDidUpdate({location}) {
+      if (location && location.hash && location.hash.length) {
+        return;
+      }
+
+      mediumZoom(className, {
+        // This is the height of the nav header
+        margin: 51,
+        background: 'rgb(50, 50, 50)'
+      })
+    }
+  }
+})()
+
