700fcd0b6b1e3e618e702926f253898806421bdf
Federico Maggi <federico.maggi@mia-platform.eu>
2023-05-18 16:56:33 +0200

    ci: console info in build redirects

--- scripts/build-permanent-redirects.js
+++ scripts/build-permanent-redirects.js
@@ -1,9 +1,15 @@
+/* eslint-disable no-console */
+
 const fs = require('fs');
 const path = require('path');
 
 const raw_redirects = require('../301redirects.json');
 
+console.log('Generating permanent redirects');
+
 const output = path.join(__dirname, '..', 'nginx', 'permanent.redirects');
 const redirects = Object.keys(raw_redirects).map((key) => `rewrite ^${key}$ ${raw_redirects[key].destination} permanent;`);
+
+console.log(`Generated ${redirects.length} permanent redirects`);
 
 fs.writeFileSync(output, redirects.join("\n"));
