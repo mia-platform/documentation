4417a9d3375a1d4d91a8dfe0386136ac7d7e788c
Giuseppe Manzi <giuseppe.manzi@mia-platform.eu>
2023-11-29 09:54:28 +0100

    [Home Page] Group last pages by category

--- src/components/LastPages/index.js
+++ src/components/LastPages/index.js
@@ -20,15 +20,33 @@ LastPagesLink.propTypes = {
 }
 
 function LastPages({title, description}) {
-    const linksToShow = links.slice(0,23);
+    const pagesToShow = links.pages.slice(0, 23);
+    const others = pagesToShow
+            .filter(({url}) => !links.categories.some(({prefix}) => url.startsWith(prefix)))
+            .map((props, idx) => (<LastPagesLink key={idx} {...props} />))
     return (
         <div className={clsx("col col--12", featureStyle.feature, styles.howToBox)}>
             <h3>{title}</h3>
             <p>{description}</p>
             <div className={styles.howToLinkBox}>
-                {linksToShow.map((props, idx) => (
-                    <LastPagesLink key={idx} {...props} />
-                ))}
+                {
+                    links.categories.map(({title, prefix}, categoryId) => {
+                        const linksComponents = pagesToShow
+                            .filter((({url}) => url.startsWith(prefix)))
+                            .map((props, idx) => (<LastPagesLink key={idx} {...props} />))
+                        return linksComponents.length > 0 ? (
+                            <div className={clsx("row", styles.howToCategoryBox)} key={categoryId}>
+                                <h4>{title}</h4>
+                                {linksComponents}
+                            </div>
+                        ): undefined
+                    }).concat(
+                            <div className={clsx("row", styles.howToCategoryBox)} key={links.categories.length}>
+                                <h4>{"Others"}</h4>
+                                {others}
+                            </div>
+                    )
+                }
             </div>
         </div>
     );
