059fca0eb1c8515266629dafa114fd002d11bd7d
Giuseppe Manzi <giuseppe.manzi@mia-platform.eu>
2023-11-29 11:24:33 +0100

    code refactor

--- src/components/LastPages/index.js
+++ src/components/LastPages/index.js
@@ -6,6 +6,21 @@ import styles from "./styles.module.css";
 import featureStyle from "../Feature/styles.module.css";
 import links from "./links.json";
 
+function buildSortBy(order) {
+    return (a, b) => {
+        if (order.indexOf(a) < 0 && order.indexOf(b) < 0) { return 0 }
+        if (order.indexOf(a) < 0) { return 1 }
+        if (order.indexOf(b) < 0) { return -1 }
+        return order.indexOf(a) - order.indexOf(b)
+    }
+}
+
+function buildFindMatchingCategory() {
+    return ({url}) => {
+        const matchingCategory = links.categories.find(({prefix}) => url.startsWith(prefix))
+        return matchingCategory?.title || 'Others'
+    }
+}
 function LastPagesLink({title, url}) {
     return (
         <div className={clsx("col col--12", "howToLink", styles.howToLink)}>
@@ -21,31 +36,25 @@ LastPagesLink.propTypes = {
 
 function LastPages({title, description}) {
     const pagesToShow = links.pages.slice(0, 23);
-    const others = pagesToShow
-            .filter(({url}) => !links.categories.some(({prefix}) => url.startsWith(prefix)))
-            .map((props, idx) => (<LastPagesLink key={idx} {...props} />))
+    const categoriesOrder = links.categories.map(({title}) => title)
+    const pagesByCategory = Object.groupBy(pagesToShow, buildFindMatchingCategory())
     return (
         <div className={clsx("col col--12", featureStyle.feature, styles.howToBox)}>
             <h3>{title}</h3>
             <p>{description}</p>
             <div className={styles.howToLinkBox}>
                 {
-                    links.categories.map(({title, prefix}, categoryId) => {
-                        const linksComponents = pagesToShow
-                            .filter((({url}) => url.startsWith(prefix)))
-                            .map((props, idx) => (<LastPagesLink key={idx} {...props} />))
-                        return linksComponents.length > 0 ? (
-                            <div className={clsx("row", styles.howToCategoryBox)} key={categoryId}>
-                                <h4>{title}</h4>
-                                {linksComponents}
-                            </div>
-                        ): undefined
-                    }).concat(
-                            <div className={clsx("row", styles.howToCategoryBox)} key={links.categories.length}>
-                                <h4>{"Others"}</h4>
-                                {others}
-                            </div>
-                    )
+                    Object.keys(pagesByCategory)
+                        .sort(buildSortBy(categoriesOrder))
+                        .map((title, categoryId)=> {
+                            const linksComponents =  pagesByCategory[title].map((props, idx) => (<LastPagesLink key={idx} {...props} />))
+                            return linksComponents.length > 0 ? (
+                                <div className={clsx("row", styles.howToCategoryBox)} key={categoryId}>
+                                    <h4>{title}</h4>
+                                    {linksComponents}
+                                </div>
+                            ): undefined
+                        })
                 }
             </div>
         </div>
