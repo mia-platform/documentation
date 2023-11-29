a83127c4ed67812de918b04bf5e371b2f713fc71
Giuseppe Manzi <giuseppe.manzi@mia-platform.eu>
2023-11-29 16:04:43 +0100

    code refactor

--- src/components/LastPages/index.js
+++ src/components/LastPages/index.js
@@ -8,7 +8,7 @@ import links from "./links.json";
 
 function buildFindMatchingCategory(defaultCategory) {
     return ({url}) => {
-        const matchingCategory = links.categories.find(({prefix}) => url.startsWith(prefix))
+        const matchingCategory = links.categories.find(({prefixes}) => prefixes.some(prefix => url.startsWith(prefix)))
         return matchingCategory?.title || defaultCategory
     }
 }
@@ -27,33 +27,32 @@ LastPagesLink.propTypes = {
 
 function LastPages({title, description}) {
     const pagesToShow = links.pages.slice(0, 23);
-    const categoriesOrder = links.categories.map(({title}) => title)
     const defaultCategory = 'Others'
+    const categoriesOrder = links.categories.map(({title}) => title).concat(defaultCategory)
     const pagesByCategory = Object.groupBy(pagesToShow, buildFindMatchingCategory(defaultCategory))
     return (
         <div className={clsx("col col--12", featureStyle.feature, styles.howToBox)}>
-            <h3>{title}</h3>
-            <p>{description}</p>
-            <div className={styles.howToLinkBox}>
-                {
-                    categoriesOrder
-                        .concat(defaultCategory)
-                        .map((title, categoryId) => {
-                            const pagesOfCurrentCategory = pagesByCategory[title]
-                            if (!pagesOfCurrentCategory) {
-                                return
-                            }
-                            const linksComponents = pagesOfCurrentCategory.map((props, idx) => (<LastPagesLink key={idx} {...props} />))
-                            return (
-                                <div className={clsx("row", styles.howToCategoryBox)} key={categoryId}>
-                                    <h4>{title}</h4>
-                                    {linksComponents}
-                                </div>
-                            )
-                        })
-                }
+        <h3>{title}</h3>
+        <p>{description}</p>
+                <div className={styles.howToLinkBox}>
+                    {
+                        categoriesOrder
+                            .map((title, categoryId) => {
+                                const pagesOfCurrentCategory = pagesByCategory[title]
+                                if (!pagesOfCurrentCategory) {
+                                    return
+                                }
+                                const linksComponents = pagesOfCurrentCategory.map((props, idx) => (<LastPagesLink key={idx} {...props} />))
+                                return (
+                                    <div className={clsx("row", styles.howToCategoryBox)} key={categoryId}>
+                                        <h4>{title}</h4>
+                                        {linksComponents}
+                                    </div>
+                                )
+                            })
+                    }
+                </div>
             </div>
-        </div>
     );
 }
 
