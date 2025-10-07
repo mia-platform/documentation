25cf02d8760fb71e54a8d897f63788a4349b10c3
Paola Nicosia <paola.nicosia@mia-platform.eu>
2025-10-07 16:00:44 +0200

    Moved v13 RN

--- sidebarsReleaseNotes.js
+++ sidebarsReleaseNotes.js
@@ -4,7 +4,7 @@ export default {
     {
       "label": "v14",
       "type": "category",
-      "collapsed": true,
+      "collapsed": false,
       "link": {
         "type": "doc",
         "id": "v14-overview"
@@ -19,6 +19,53 @@ export default {
         {"id": "v14.0.2", "type": "doc"},
         {"id": "v14.0.1", "type": "doc"},
         {"id": "v14.0.0", "type": "doc"}
+      ]
+    },
+    {
+      "label": "v13",
+      "type": "category",
+      "collapsed": true,
+      "link": {
+        "type": "doc",
+        "id": "v13/v13-overview"
+      },
+      "items": [
+        {"id": "v13/versions", "type": "doc"},
+        {"id": "v13/v13.7.5", "type": "doc"},
+        {"id": "v13/v13.7.4", "type": "doc"},
+        {"id": "v13/v13.7.3", "type": "doc"},
+        {"id": "v13/v13.7.2", "type": "doc"},
+        {"id": "v13/v13.7.1", "type": "doc"},
+        {"id": "v13/v13.7.0", "type": "doc"},
+        {"id": "v13/v13.6.2", "type": "doc"},
+        {"id": "v13/v13.6.1", "type": "doc"},
+        {"id": "v13/v13.6.0", "type": "doc"},
+        {"id": "v13/v13.5.2", "type": "doc"},
+        {"id": "v13/v13.5.1", "type": "doc"},
+        {"id": "v13/v13.5.0", "type": "doc"},
+        {"id": "v13/v13.4.0", "type": "doc"},
+        {"id": "v13/v13.3.5", "type": "doc"},
+        {"id": "v13/v13.3.4", "type": "doc"},
+        {"id": "v13/v13.3.3", "type": "doc"},
+        {"id": "v13/v13.3.2", "type": "doc"},
+        {"id": "v13/v13.3.1", "type": "doc"},
+        {"id": "v13/v13.3.0", "type": "doc"},
+        {"id": "v13/v13.2.1", "type": "doc"},
+        {"id": "v13/v13.2.0", "type": "doc"},
+        {"id": "v13/v13.1.4", "type": "doc"},
+        {"id": "v13/v13.1.3", "type": "doc"},
+        {"id": "v13/v13.1.2", "type": "doc"},
+        {"id": "v13/v13.1.1", "type": "doc"},
+        {"id": "v13/v13.1.0", "type": "doc"},
+        {"id": "v13/v13.0.8", "type": "doc"},
+        {"id": "v13/v13.0.7", "type": "doc"},
+        {"id": "v13/v13.0.6", "type": "doc"},
+        {"id": "v13/v13.0.5", "type": "doc"},
+        {"id": "v13/v13.0.4", "type": "doc"},
+        {"id": "v13/v13.0.3", "type": "doc"},
+        {"id": "v13/v13.0.2", "type": "doc"},
+        {"id": "v13/v13.0.1", "type": "doc"},
+        {"id": "v13/v13.0.0", "type": "doc"}
       ]
     },
     {
